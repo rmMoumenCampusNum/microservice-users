a3b9373915910d4c4afa940fe0e583ff3ff95b15
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-11-13 11:35:53 +0100

    retrait de la sécurité

--- src/main/java/com/ebm/users/UsersApplication.java
+++ src/main/java/com/ebm/users/UsersApplication.java
@@ -1,57 +1,20 @@
 package com.ebm.users;
 
-import com.ebm.users.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 
 @SpringBootApplication
 @Configuration
-@EnableWebSecurity
 public class UsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
-	}
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-				.cors(Customizer.withDefaults())
-				.csrf(AbstractHttpConfigurer::disable) // Désactive CSRF pour les appels d'API REST
-				.authorizeHttpRequests(auth -> {
-					// Accès à tous les utilisateurs connectés (USER ou ADMIN) pour ces routes
-					auth.requestMatchers(HttpMethod.GET, "/users", "users/{id}").hasRole("ADMIN");
-					auth.requestMatchers(HttpMethod.GET, "/users/{id}").hasRole("USER");
-
-					// Autoriser le POST sans authentification pour la création d'un utilisateur
-					auth.requestMatchers(HttpMethod.POST, "/users").permitAll();
-
-					auth.requestMatchers(HttpMethod.POST, "/users/login").permitAll();
-
-					// Toute autre requête nécessite une authentification
-					auth.anyRequest().authenticated();
-				})
-				.httpBasic(Customizer.withDefaults())
-				.build();
 	}
 
 
@@ -60,28 +23,4 @@ public class UsersApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Autowired
-	private UserService customUserDetailsService;
-
-	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
-		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-		authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
-		return authenticationManagerBuilder.build();
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Ajout de OPTIONS
-						.allowedHeaders("*")
-						.allowCredentials(true)  // Important pour les requêtes authentifiées
-						.maxAge(3600);  // Cache des pre-flight requests
-			}
-		};
-	}
 }
