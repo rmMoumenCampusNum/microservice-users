3b08f9671c3bebd9e92a5b7608048854b3532ec5
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-11-14 11:39:27 +0100

    ajout de swagger

--- src/main/java/com/ebm/users/UsersApplication.java
+++ src/main/java/com/ebm/users/UsersApplication.java
@@ -2,18 +2,23 @@ package com.ebm.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 
-
-
 @SpringBootApplication
 @Configuration
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class UsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
 }
+
+
+
--- src/main/java/com/ebm/users/web/controller/UserController.java
+++ src/main/java/com/ebm/users/web/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ebm.users.web.controller;
 
 import com.ebm.users.service.UserService;
 import com.ebm.users.web.model.User;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +16,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Récupérer tous les users", description = "Retourne tous les users")
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> user = userService.getAllUsers();
         return ResponseEntity.ok(user);
     }
 
+    @Operation(summary = "Récupérer un user", description = "Retourne un user par son ID")
     @GetMapping("/users/{id}")
     public ResponseEntity<Optional<User>> getUserById(@PathVariable long id) {
         Optional<User> user = userService.getUserById(id);
         return ResponseEntity.ok(user);
     }
 
+    @Operation(summary = "Créer un user", description = "Crée un user")
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User userCreated = userService.createUser(user);
